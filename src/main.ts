ea4d517b80f5727799932d924c31810db6ac93b2
William Sanders <253146473+wsandersvc@users.noreply.github.com>
2026-03-18 12:57:26 -0500

    Initial release

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import fs from 'fs'
+import yaml from 'js-yaml'
 
 /**
  * The main function for the action.
@@ -8,18 +9,46 @@ import { wait } from './wait.js'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const mapping_path = core.getInput('runs-on-mapping-yaml', { required: true })
+    const repository = core.getInput('repository', { required: true })
+    const default_runs_on = core.getInput('default-runs-on', { required: true })
 
+    core.info(`Loading runs-on-mapping-yaml from ${mapping_path}`)
+    const file_content = await fs.promises
+      .readFile(mapping_path, 'utf-8')
+      .catch((error) => {
+        if (error.code === 'ENOENT') {
+          core.error(`Mapping file not found: ${mapping_path}`)
+        } else {
+          core.error(`Failed to read mapping file: ${error.message}`)
+        }
+        throw error
+      })
+
+    // throws YAMLException on failure
+    const mapping_yaml = yaml.load(file_content) as { [runs_on: string]: string[] }
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`mapping_yaml type: ${typeof mapping_yaml}`)
+    core.debug(`mapping_yaml keys: ${Object.keys(mapping_yaml)}`)
+    core.debug(`mapping_yaml schema: ${JSON.stringify(mapping_yaml, null, 2)}`)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let runs_on = default_runs_on
+    for (const [key, repositories] of Object.entries(mapping_yaml)) {
+      // validate runs-on key contains an array
+      if (!Array.isArray(repositories)) {
+        core.warning(`Skipping key "${key}": value is not an array`)
+        continue
+      }
+
+      if (repositories.includes(repository)) {
+        runs_on = key
+        core.info(`Found repository "${repository}" in runs_on group: ${key}`)
+        break
+      }
+    }
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('runs_on', runs_on)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
